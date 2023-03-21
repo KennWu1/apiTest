c3431510cce1259eac27c1fca7bbd85005fee204
KennWooo <124717342+KennWooo@users.noreply.github.com>
2023-03-21 13:12:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-const API_KEY = '34611318-dbbdf339c5ee9fd36c6360e2c';
-const BASE_URL = `https://pixabay.com/api/?key=${API_KEY}&image_type=photo&per_page=200`;
+const apiKey = '34611318-dbbdf339c5ee9fd36c6360e2c';
 
-const imageContainer = document.querySelector('.image-container');
-const randomImageButton = document.querySelector('#generate-button');
+const generateBtn = document.getElementById('generate-btn');
+const parameterInput = document.getElementById('parameter-input');
+const image = document.getElementById('image');
 
-randomImageButton.addEventListener('click', () => {
-  fetch(BASE_URL)
+generateBtn.addEventListener('click', () => {
+  const parameter = parameterInput.value.trim(); // Remove leading/trailing spaces
+  const url = `https://pixabay.com/api/?key=${apiKey}&q=${parameter}&image_type=photo&orientation=horizontal&per_page=200`;
+  
+  fetch(url)
     .then(response => response.json())
     .then(data => {
       const randomIndex = Math.floor(Math.random() * data.hits.length);
-      const randomImageURL = data.hits[randomIndex].webformatURL;
-      imageContainer.innerHTML = `<img id="random-image" src="${randomImageURL}" alt="Random Image">`;
+      const randomImage = data.hits[randomIndex];
+      image.src = randomImage.largeImageURL;
     })
     .catch(error => console.error(error));
 });
